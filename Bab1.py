27922d56fd6c12c1f76db75538770852a6684f97
syifa khairiyah <fauzan123562@gmail.com>
2025-05-02 22:21:05 +0700

    Statel 2

--- Bab1.py
+++ Bab1.py
@@ -2,18 +2,24 @@ import math
 import matplotlib.pyplot as peta
 from collections import Counter
 
+#Nilai Final Exam Statistika Elementer
 dataD = [
-    10, 15, 17, 23, 25, 32, 34, 36, 41, 41,
-    43, 48, 52, 52, 54, 55, 57, 60, 60, 61,
-    62, 63, 64, 64, 65, 67, 67, 69, 70, 71,
-    72, 74, 74, 74, 75, 76, 76, 77, 78, 78,
-    79, 79, 80, 80, 80, 81, 81, 82, 82, 83,
-    84, 84, 85, 85, 88, 89, 90, 92, 95, 98
-]
+    23, 60, 79, 32, 57, 74, 52, 70, 82, 36,
+    80, 77, 81, 95, 41, 65, 92, 85, 55, 76,
+    52, 10, 64, 75, 78, 25, 80, 98, 81, 67,
+    41, 71, 83, 54, 64, 72, 88, 62, 74, 43,
+    60, 78, 89, 76, 84, 48, 84, 90, 15, 79, 
+    34, 67, 17, 82, 69, 74, 63, 80, 85, 61
+    ]
 
-dataC = [2.2,4.1,3.5,4.5,3.2,3.7,3.0,2.6,3.4,1.6,3.1,3.3,3.8,3.1,4.7,3.7,2.5,4.3
-         ,3.4,3.6,2.9,3.3,3.9,3.1,3.3,3.1,3.7,4.4,3.2,4.1,1.9,3.4,4.7,3.8,3.2,
-         2.6,3.9,3.0,4.2,3.5]
+
+#Nilai 
+dataC = [
+    2.2, 4.1, 3.5, 4.5, 3.2, 3.7, 3.0, 2.6, 3.4, 1.6,
+    3.1, 3.3, 3.8, 3.1, 4.7, 3.7, 2.5, 4.3, 3.4, 3.6,
+    2.9, 3.3, 3.9, 3.1, 3.3, 3.1, 3.7, 4.4, 3.2, 4.1,
+    1.9, 3.4, 4.7, 3.8, 3.2, 2.6, 3.9, 3.0, 4.2, 3.5
+    ]
 
 
 
@@ -46,6 +52,15 @@ def modus(totalData) :
             bAngkaDanAngka = (bAngka,nomor)
     return bAngkaDanAngka[1]
 
+def mode(totalData):
+    frekuensi = {}
+    for angka in totalData:
+        frekuensi[angka] = frekuensi.get(angka, 0) + 1
+
+    maksFrekuensi = max(frekuensi.values())
+    modusList = [angka for angka, jumlah in frekuensi.items() if jumlah == maksFrekuensi]
+
+    return modusList
 
 
 #Coding untuk median
@@ -56,20 +71,20 @@ def median(totalData) :
         return totalData[lenBAngka]
 
     elif len(totalData) % 2 == 0 :
-        lenBAngka1 = int(len(totalData)/ 2)
-        lenBAngka2 = int(len(totalData)/ 2) + 1
+        lenBAngka1 = int(len(totalData)/ 2) - 1
+        lenBAngka2 = int(len(totalData)/ 2)
         hasilMean  = (totalData[lenBAngka1] + totalData[lenBAngka2]) / 2
         return hasilMean
 
 
 
 #Coding untuk nilai min
-def min(totalData) :
+def nilaiMin(totalData) :
     totalData.sort()
     return totalData[0]
 
 #Coding untuk nilai max
-def max(totalData) :
+def nilaiMax(totalData) :
     totalData.sort()
     l = len(totalData) - 1
     return totalData[l]
@@ -102,7 +117,7 @@ def sDeviasi (totalData) :
 
 
 #Coding untuk jumlah
-def sum(totalData) :
+def jumlah(totalData) :
     jumlah = 0
     for angka in totalData :
         jumlah = jumlah + angka
@@ -110,13 +125,13 @@ def sum(totalData) :
 
 
 #Coding untuk histogram
-#peta.hist(data,edgecolor="white")
+#peta.hist(dataD,edgecolor="white")
 #peta.xlabel("HP")
 #peta.ylabel("Banyak Mobil")
 #peta.title("Car Battery Life")
 
 #Coding box plot
-#peta.boxplot(data)
+#peta.boxplot(dataD)
 
 #peta.show()
 
@@ -144,7 +159,7 @@ def stemPlotC(totalData) :
 
     for number in totalData:
         stem = int(number)
-        leaf = int(round((number - stem) * 10))  # ambil angka di belakang koma
+        leaf = int(round((number - stem) * 10))  
         stem_leaf.setdefault(stem, []).append(leaf)
 
     # Cetak stem plot
@@ -162,4 +177,7 @@ def tabelFrekuensi(totalData) :
     print("Nilai\tFrekuensi")
     for value in sorted(frekuensi):
         print(f"{value}\t{frekuensi[value]}")
+
+
+
 
